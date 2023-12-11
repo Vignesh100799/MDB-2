ad92e125e39b92d90f2b3e475242e920f5153844
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-11 14:39:16 +0530

    Changes

--- queries.js
+++ queries.js
@@ -62,84 +62,74 @@ db.topics.aggregate([
 /*Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020*/
 
 db.company_drives.find({
-    $or: [
-      { drive_date: { $gte: new Date("15-oct-2020") } },
-      { drive_date: { $lte: new Date("31-0ct-2020") } },
-    ],
-  });
+  "drive_date": {
+    $gte: new Date("2023-10-15"),
+    $lte: new Date("2023-10-31")
+  }
+});
 
 
 
   /*Find all the company drives and students who are appeared for the placement.*/
 
 db.company_drives.aggregate([
-    {
-      $lookup: {
-        from: "users",
-        localField: "userid",
-        foreignField: "userid",
-        as: "userinfo",
-      },
+  {
+    $lookup: {
+      from: "users",
+      localField: "user_id",
+      foreignField: "user_id",
+      as: "users",
     },
-    {
-      $project: {
-        _id: 0,
-        "userinfo.name": 1,
-        company: 1,
-        drive_date: 1,
-        "userinfo.email": 1,
-        "userinfo.userid": 1,
-      },
+  },
+{
+  $unwind: "$users",
+},
+  {
+    $project: {
+      _id: 0,
+      "users.user_name": 1,
+      company: 1,
+      drive_date: 1,
+      "users.user_email": 1,
+      "users.user_id": 1,
     },
-  ]);
+  },
+]);
   
 
 
   /*Find the number of problems solved by the user in codekata*/
 
 db.codekata.aggregate([
-    {
-      $lookup: {
-        from: "users",
-        localField: "userid",
-        foreignField: "userid",
-        as: "userinfo",
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        userid: 1,
-        problems: 1,
-        "userinfo.name": 1,
-      },
-    },
-  ]);
+  {
+    $lookup: {
+      from: "users",
+      localField: "user_id",
+      foreignField: "user_id",
+      as: "user"
+    }
+  },
+  {
+    $unwind: "$user"
+  },
+  {
+    $project: {
+      _id: 0,
+      user_id: "$user.user_id",
+      user_name: "$user.user_name",
+      problems_solved: "$problems"
+    }
+  }
+]);
 
 
 
   /*Find all the mentors with who has the mentee's count more than 15*/
-
-db.users.aggregate([
-    {
-      $lookup: {
-        from: "mentors",
-        localField: "mentorid",
-        foreignField: "mentorid",
-        as: "mentorInfo",
-      },
-    },
-    {
-      $group: {
-        _id: {
-          mentorid: "$mentorInfo.mentorid",
-          mentorname: "$mentorInfo.mentorname",
-        },
-        mentee_count: { $sum: 1 },
-      },
-    },
-    { $match: { mentee_count: { $gt: 15 } } },
-  ]);
+  db.mentors.find({
+    "mentor_count": {
+      $gt: 15
+    }
+  })
 
 
 
@@ -147,42 +137,42 @@ db.users.aggregate([
   /*Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020*/
 
 db.attendance.aggregate([
-    {
-      $lookup: {
-        from: "topics",
-        localField: "topicid",
-        foreignField: "topicid",
-        as: "topics",
-      },
+  {
+    $lookup: {
+      from: "topics",
+      localField: "topic_id",
+      foreignField: "topic_id",
+      as: "topics",
     },
-    {
-      $lookup: {
-        from: "tasks",
-        localField: "topicid",
-        foreignField: "topicid",
-        as: "tasks",
-      },
+  },
+  {
+    $lookup: {
+      from: "tasks",
+      localField: "topic_id",
+      foreignField: "topic_id",
+      as: "tasks",
     },
-    { $match: { $and: [{ attended: false }, { "tasks.submitted": false }] } },
-    {
-      $match: {
-        $and: [
-          {
-            $or: [
-              { "topics.topic_date": { $gte: new Date("15-oct-2020") } },
-              { "topics.topic_date": { $lte: new Date("31-oct-2020") } },
-            ],
-          },
-          {
-            $or: [
-              { "tasks.due_date": { $gte: new Date("15-oct-2020") } },
-              { "tasks.due_date": { $lte: new Date("31-oct-2020") } },
-            ],
-          },
-        ],
-      },
+  },
+  { $match: { $and: [{ attend: false }, { "tasks.submitted": false }] } },
+  {
+    $match: {
+      $and: [
+        {
+          $or: [
+            { "topics.topic_date": { $gte: new Date("15-oct-2023") } },
+            { "topics.topic_date": { $lte: new Date("31-oct-2023") } },
+          ],
+        },
+        {
+          $or: [
+            { "tasks.due_date": { $gte: new Date("15-oct-2023") } },
+            { "tasks.due_date": { $lte: new Date("31-oct-2023") } },
+          ],
+        },
+      ],
     },
-    {
-      $count: "No_of_students_absent",
-    },
-  ]);
+  },
+  {
+    $count: "No_of_students_absent",
+  },
+]);
